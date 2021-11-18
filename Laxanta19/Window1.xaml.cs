970d350c82cbbcfc80d17ddb219b3b18a24bfd28
Студенты Кузьминой О.Б <kuzob@teach.local>
2021-11-18 12:54:10 +0300

    Добавьте файлы проекта.

---
+++ Laxanta19/Window1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace Laxanta19
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            
+            string connStr = "server=192.168.4.211; user=student; database=dragunov431; password=12345; ";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            MySqlDataAdapter ad = new MySqlDataAdapter();
+            MySqlCommand com = new MySqlCommand("SELECT * FROM `material_b_import` WHERE 1", conn);
+            ad.SelectCommand = com;
+
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            GdGrid.ItemsSource = dt.DefaultView;
+        }
+
+        private void GdGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow myForm = new MainWindow();
+            this.Hide();
+            myForm.ShowDialog();
+        }
+    }
+}
